b3368b6ce094a34cd79e21ec838659b2d0751736
Brian-Zavala <brian.zavala25@proton.me>
2025-03-06 21:56:29 -0600

    Added MainLayout.jsx in layout dir

---
+++ client/src/components/layout/MainLayout.jsx
@@ -0,0 +1,22 @@
+// src/components/layout/MainLayout.jsx
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './Header';
+import Footer from './Footer';
+
+const MainLayout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+      <ToastContainer position="bottom-right" />
+    </div>
+  );
+};
+
+export default MainLayout;
