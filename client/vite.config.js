d05f14cbeb2638848c3167774180f58929f20b6f
Brian-Zavala <brian.zavala25@proton.me>
2025-03-06 21:55:34 -0600

    Added Footer.jsx to layout dir in client/src

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
